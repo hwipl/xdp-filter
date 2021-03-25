69b32a74fc6af475a4b8a051efdafd645c03371f
hwipl <33433250+hwipl@users.noreply.github.com>
2021-03-25 19:29:57 +0100

    add xdp_filter_kern.c
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ xdp_filter_kern.c
@@ -0,0 +1,55 @@
+/* bpf */
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+
+/* ethernet */
+#include <linux/if_ether.h>
+
+/* set license to gpl */
+char _license[] SEC("license") = "GPL";
+
+/* map definitions */
+struct bpf_elf_map {
+	__u32 type;
+	__u32 size_key;
+	__u32 size_value;
+	__u32 max_elem;
+	__u32 flags;
+	__u32 id;
+	__u32 pinning;
+};
+
+/* map for source macs */
+#define MAX_SRC_MACS 1024
+struct bpf_elf_map SEC("maps") src_macs = {
+	.type = BPF_MAP_TYPE_HASH,
+	.size_key = ETH_ALEN,
+	.size_value = sizeof(char),
+	.max_elem = MAX_SRC_MACS,
+};
+
+/* count all packets */
+SEC("filter_ethernet")
+int _filter_ethernet(struct xdp_md *ctx)
+{
+	void *data_end = (void *)(long)ctx->data_end;
+	void *data = (void *)(long)ctx->data;
+	struct ethhdr *eth = data;
+	__u32 key = 0;
+	__u64 nh_off;
+	long *value;
+
+	/* check packet length for verifier */
+	if (data + sizeof(struct ethhdr) > data_end) {
+		return XDP_PASS;
+	}
+
+	/* check if src mac is in src_macs map */
+	value = bpf_map_lookup_elem(&src_macs, eth->h_source);
+	if (value) {
+		/* found src mac, drop packet */
+		return XDP_DROP;
+	}
+
+	return XDP_PASS;
+}
