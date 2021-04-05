28399de98b6f51636941d1a97d3539915e912184
hwipl <33433250+hwipl@users.noreply.github.com>
2021-04-05 20:57:52 +0200

    build.sh: add target bpf
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-clang -O2 -emit-llvm -c xdp_filter_kern.c -o - -fno-stack-protector |
+clang -O2 -emit-llvm -c xdp_filter_kern.c -o - -fno-stack-protector \
+	-target bpf |
 	llc -march=bpf -filetype=obj -o xdp_filter_kern.o
 clang xdp_filter_user.c -o xdp_filter_user -l bpf
